tfherbert/dpdk-snap

commit 4821395c6a5ff3a26367071abd3c9e7216eea45f

- Avoid variable expansion in the spec here-documents during build

dpdk.spec
@@ -1,6 +1,6 @@
 Name: dpdk
 Version: 1.7.0 
-Release: 5%{?dist}
+Release: 6%{?dist}
 URL: http://dpdk.org
 Source: http://dpdk.org/browse/dpdk/snapshot/dpdk-%{version}.tar.gz
 
@@ -96,7 +96,7 @@ cp -a  mk/                   %{buildroot}%{sdkdir}
 # Setup RTE_SDK environment as expected by apps etc
 mkdir -p %{buildroot}/%{_sysconfdir}/profile.d
 cat << EOF > %{buildroot}/%{_sysconfdir}/profile.d/dpdk-sdk-%{_arch}.sh
-if [ -z "${RTE_SDK}" ]; then
+if [ -z "\${RTE_SDK}" ]; then
     export RTE_SDK="%{sdkdir}"
     export RTE_TARGET="%{target}"
     export RTE_INCLUDE="%{_includedir}/%{name}-%{version}"
@@ -104,7 +104,7 @@ fi
 EOF
 
 cat << EOF > %{buildroot}/%{_sysconfdir}/profile.d/dpdk-sdk-%{_arch}.csh
-if ( ! $RTE_SDK ) then
+if ( ! \$RTE_SDK ) then
     setenv RTE_SDK "%{sdkdir}"
     setenv RTE_TARGET "%{target}"
     setenv RTE_INCLUDE "%{_includedir}/%{name}-%{version}"
@@ -137,6 +137,9 @@ find %{buildroot}%{_includedir}/%{name}-%{version} -type f | xargs chmod 0644
 %{_sysconfdir}/profile.d/dpdk-sdk-*.*
 
 %changelog
+* Wed Jan 27 2015 Panu Matilainen <pmatilai@redhat.com> - 1.7.0-6
+- Avoid variable expansion in the spec here-documents during build
+
 * Tue Jan 27 2015 Panu Matilainen <pmatilai@redhat.com> - 1.7.0-5
 - Avoid unnecessary use of %%global, lazy expansion is normally better
 - Drop unused destdir macro while at it

fedora-riscv/dpdk

commit 83d917e6443791b1bedf961da8a5ef01019a941b

fix test for RTE_SDK variable in csh profile
In csh, undefined variables do not expand to the empty string.  Use the
$? csh substitution to check if the variable is defined.

Fixes: 4821395c6a5f ("- Avoid variable expansion in the spec here-documents during build")

dpdk.spec
@@ -255,7 +255,7 @@ fi
 EOF
 
 cat << EOF > %{buildroot}/%{_sysconfdir}/profile.d/dpdk-sdk-%{_arch}.csh
-if ( ! \$RTE_SDK ) then
+if ( ! \$?RTE_SDK ) then
     setenv RTE_SDK "%{sdkdir}"
     setenv RTE_TARGET "%{target}"
     setenv RTE_INCLUDE "%{incdir}"